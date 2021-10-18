4666e1c50263bc39ddebabc58578216c787c67a1
KevinJayke <80684904+KevinJayke@users.noreply.github.com>
2021-10-18 14:28:28 +0200

    pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -3,12 +3,12 @@ const Pagination = ({page, setpage}) => {
     return (
         <nav aria-label="Page navigation example">
             <ul className="pagination justify-content-center">
-                <li className="page-item disabled">
-                    <a className="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                <li className={"page-item" + (page=== 1 ? "disabled":null)}>
+                    <a className="page-link" onClick={() => page > 1 ? setpage(page -1) : null} href="#">Previous</a>
                 </li>
-                <li className="page-item"><a className="page-link" href="#">{page+1}</a></li>
-                <li className="page-item"><a className="page-link" href="#">{page+2}</a></li>
-                <li className="page-item"><a className="page-link" href="#">{page+3}</a></li>
+                <li className="page-item"><a className="page-link" onClick={() => setpage(page)} href="#">{page}</a></li>
+                <li className="page-item"><a className="page-link" onClick={() => setpage(page+1)} href="#">{page+2}</a></li>
+                <li className="page-item"><a className="page-link" onClick={() => setpage(page+2)} href="#">{page+3}</a></li>
                 <li className="page-item">
                     <a className="page-link" onClick={() => setpage(page+1) } href="#">Next</a>
                 </li>
